24686d7ff03ff1c2cd8864280c79a0af524d3780
Ats Nisov <ats.nisov@googlemail.com>
2016-01-06 12:34:46 +0200

    The example server now returns the file url.

--- example_project/file_uploads/views.py
+++ example_project/file_uploads/views.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 
 from django.http import Http404, HttpResponse
 from django.views.generic import View
@@ -6,15 +7,15 @@ from django.views.generic import View
 
 class UploadView(View):
     def post(self, request):
+        assert len(self.request.FILES) == 1
+
         from django.core.files.storage import default_storage
 
-        file_urls = []
+        field_name, tmp_file = self.request.FILES.items()[0]
+        with default_storage.open("user_uploads/" + tmp_file.name + "-" + str(datetime.datetime.now()), "wb+") as f:
+            for chunk in tmp_file.chunks():
+                f.write(chunk)
 
-        for field_name, tmp_file in self.request.FILES.items():
-            with default_storage.open("user_uploads/" + tmp_file.name + "-" + str(datetime.datetime.now()), "wb+") as f:
-                for chunk in tmp_file.chunks():
-                    f.write(chunk)
+            file_url = default_storage.url(f.name)
 
-                file_urls.append(default_storage.url(f.name))
-
-        return HttpResponse("")
+        return HttpResponse(json.dumps({"file_url": file_url}))
