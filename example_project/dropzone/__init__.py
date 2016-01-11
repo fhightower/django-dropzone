e2b0352b7c3d84f17238494d4a09005c8c6ca488
Ats Nisov <ats.nisov@googlemail.com>
2016-01-11 11:41:31 +0200

    No need to copy the dropzone module to example_project every time there is a change anymore.

--- example_project/dropzone/__init__.py
+++
@@ -1,2 +0,0 @@
-__author__ = 'chrisvilla, Ats Nisov (github.com/Rubinous)'
-__version__ = "0.2.0"
--- example_project/manage.py
+++ example_project/manage.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
+import inspect
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_project.settings")
+
+    currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    parentdir = os.path.dirname(currentdir)
+    sys.path.insert(0,parentdir)
 
     from django.core.management import execute_from_command_line
 
